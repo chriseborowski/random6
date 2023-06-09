d88272e0cae7680693c372b3420e74f8b4eca103
Chris Borowski <krzysztofeborowski@gmail.com>
2023-06-09 18:02:50 -0500

    Added congrats message in ASCII art

--- random6.py
+++ random6.py
@@ -8,6 +8,16 @@ user_number = int(input("Enter a whole number between 1 and 100: "))
 for i in range(1, 6):
   if user_number == random_number:
     print("You guessed it! Congratulations!")
+    print(r"""
+   _____                            _       _ 
+  / ____|                          | |     | |
+ | |     ___  _ __   __ _ _ __ __ _| |_ ___| |
+ | |    / _ \| '_ \ / _` | '__/ _` | __/ __| |
+ | |___| (_) | | | | (_| | | | (_| | |_\__ \_|
+  \_____\___/|_| |_|\__, |_|  \__,_|\__|___(_)
+                     __/ |                    
+                    |___/                     
+    """)
     break
   elif user_number > random_number:
     print("Too big! Try again.")
