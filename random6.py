08e85606e924c2ab41041934dddbbd1cd19185ea
Chris Borowski <krzysztofeborowski@gmail.com>
2023-06-09 17:40:39 -0500

    Added support for floats

--- random6.py
+++ random6.py
@@ -12,13 +12,11 @@ for i in range(1, 6):
   elif user_number > random_number:
     print("Too big! Try again.")
     counter -= 1
-    user_number = int(
-      input("Remaining attempts: " + str(counter) + ".\nEnter a whole number between 1 and 100: "))
+    user_number = int(float(input("Remaining attempts: " + str(counter) + ".\nEnter a whole number between 1 and 100: ")))
   elif user_number < random_number:
     print("Too small! Try again.")
     counter -= 1
-    user_number = int(
-      input("Remaining attempts: " + str(counter) + ".\nEnter a whole number between 1 and 100: "))
+    user_number = int(float(input("Remaining attempts: " + str(counter) + ".\nEnter a whole number between 1 and 100: ")))
 
 if user_number != random_number:
   print("The random number was " + str(random_number) + ". Better luck next time!")
